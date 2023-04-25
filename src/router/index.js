ffd99a31c719c2ac474f27f3b3c3bd614e4ccc19
dale <hessdale96@gmail.com>
2023-04-25 17:58:54 -0600

    adding sign up page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import ClientHomePage from '../views/ClientHomePage'
+import ClientLoginPage from '../views/ClientLoginPage'
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'clienthome',
+    component: ClientHomePage
+  }, {
+    path: `/clientlogin`,
+    name: `client log in`,
+    component: ClientLoginPage
+  }
 
 ]
 
---
+++ src/views/ClientLoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <client-signup-form></client-signup-form>
+  </div>
+</template>
+
+<script>
+import ClientSignupForm from "@/components/ClientSignupForm.vue";
+export default {
+  components: {
+    ClientSignupForm,
+  },
+};
+</script>
+
+<style scoped>
+</style>
