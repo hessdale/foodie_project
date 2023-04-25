e2c5fde1cbf5b3d93a0e0eec04c40d2c695cd965
dale <hessdale96@gmail.com>
2023-04-25 16:55:32 -0600

    tested api request with key

---
+++ src/views/TESTINGPAGE.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios
+      .request({
+        url: `https://foodie.bymoen.codes/api/validate`,
+        headers: {
+          "x-api-key": `H0x7V93WN4ebcatCvCI3`,
+        },
+      })
+      .then((response) => {
+        response;
+        document
+          .querySelector(`div`)
+          .insertAdjacentHTML(`beforeend`, `<h1>${response.data.message}<h1>`);
+      })
+      .catch((error) => {
+        error;
+        // Failure code here
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>
